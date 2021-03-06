cca5d1e1054cdbf5cdf6c6e68012c1ec440b7dcd
Kyle Loerker <matthewrshane616@gmail.com>
2021-03-06 12:35:17 -0500

    Added constants and methods for getting target.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,11 +4,15 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPipelineResult;
+import org.photonvision.PhotonTrackedTarget;
+import org.photonvision.PhotonUtils;
 
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
@@ -26,15 +30,47 @@ public class VisionSubsystem extends SubsystemBase implements Loggable {
       rowIndex = 0,
       columnIndex = 0,
       tabName = "Driver View")
-  // USB Camera Class
-  UsbCamera m_camera;
+  // Photonvision Camera
+  private PhotonCamera m_camera = new PhotonCamera("longwood564");
 
   /** Initializes the vision subsystem. */
   public VisionSubsystem() {
     // If we're runnning on the real robot, enable the camera.
     if (RobotBase.isReal()) {
-      m_camera = CameraServer.getInstance().startAutomaticCapture();
-      m_camera.setResolution(960, 720);
+      // TODO: Not necessary?
     }
   }
+
+  private PhotonPipelineResult getLatestResult() {
+    return m_camera.getLatestResult();
+  }
+
+  public boolean hasTargets() {
+    return getLatestResult().hasTargets();
+  }
+
+  /**
+   * Gets the 
+   * @return
+   */
+  public PhotonTrackedTarget getTarget() {
+    return hasTargets() ? getLatestResult().getBestTarget() : null;
+  }
+
+  /**
+   * Gets the rotation from the front-center of the robot, where the camera is, to the target. Positive values indicate a counter-clockwise rotation, to the left.
+   * @return the rotation, or null if there is no tracked target.
+   */
+  public double getRotationToTarget() {
+    return hasTargets() ? getTarget().getYaw() : null;
+  }
+
+  public double getPitchToTarget() {
+    return hasTargets() ? getTarget().getPitch() : null;
+  }
+
+  public double getDistanceToTarget() {
+    return PhotonUtils.calculateDistanceToTargetMeters(VisionConstants.kHeightCameraLow, VisionConstants.kHeightTarget, Units.degreesToRadians(VisionConstants.kPitchCameraLow), Units.degreesToRadians(getPitchToTarget()));
+  }
+
 }
